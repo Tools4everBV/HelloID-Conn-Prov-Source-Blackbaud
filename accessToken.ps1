5b10eb63836fbdeac40063b4023a702a8991cd16
mbianchi-t4e <71777580+mbianchi-t4e@users.noreply.github.com>
2021-04-20 09:45:07 -0400

    Added accessToken.ps1
    This script is used for retrieving the access token required for Blackbaud access.

---
+++ accessToken.ps1
@@ -0,0 +1,24 @@
+﻿Clear-Host
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+
+$config = @{
+	'clientId' = ''
+	'secret' = ''
+};
+
+$code = Read-Host "Visit and login to the following URL and enter in the ""code"" query parameter from the redirect, https://oauth2.sky.blackbaud.com/authorization?client_id=$($config.clientId)&response_type=code&redirect_uri=http://localhost"
+
+#Build access token request
+$tokenRequestUri = "https://oauth2.sky.blackbaud.com/token";
+    
+$headers = @{
+	'Authorization' = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $config.clientId,$config.secret)))
+	'Accept' = 'application/JSON'
+	'Content-Type' = 'application/x-www-form-urlencoded'
+};	
+
+$body = "grant_type=authorization_code&redirect_uri=http://localhost&code=$code";
+
+#Request access token
+$authResponse = Invoke-RestMethod -Method POST -Uri $tokenRequestUri -Headers $headers -Body $body -Verbose:$false;
+Write-Host "New Access Token: $($authResponse.access_token)";
